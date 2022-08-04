66d32e29f960c5dd07648ec28c2182e236a6040d
andersoncastrojb <andersoncastb@gmail.com>
2022-08-04 14:14:50 -0500

    images gallery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./components/home";
 
 function App() {
   return (
 
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path='/documen' element={<h1>Hello</h1>} />
+        </Route>
       </Routes>
+      <Footer />
+
     </div>
 
   );
---
+++ frontend/src/components/footer.js
@@ -0,0 +1,38 @@
+import "../css/footer.css"
+import pencil_img from "./img/pencil.svg"
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="columns">
+                <div className="column">
+                    <div className="box" id="box_column_one">
+                        <article className="media">
+                            <div className="media-left">
+                                <figure className="image is-64x64">
+                                <img src={pencil_img} alt="" />
+                                </figure>
+                            </div>
+                            <div className="media-content">
+                                <div className="content">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                                </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div className="column">
+                    Second column
+                </div>
+                <div className="column">
+                    Third column
+                </div>
+                <div className="column">
+                    Fourth column
+                </div>
+            </div>
+        </footer>
+    );
+}
